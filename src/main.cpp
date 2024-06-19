688b044bf580d60f5ef1c4ade227e5756a52987d
Oleg Magnes <oleg.magnes@gmail.com>
2024-06-19 22:52:13 +0200

    remove unused debug function

--- src/main.cpp
+++ src/main.cpp
@@ -15,54 +15,6 @@
 #define RUN_SHAPE_ICP 0
 #define RUN_SEQUENCE_ICP 1
 
-void debugCorrespondenceMatching()
-{
-	// Load the source and target mesh.
-	const std::string filenameSource = std::string("../../Data/bunny_part2_trans.off");
-	const std::string filenameTarget = std::string("../../Data/bunny_part1.off");
-
-	SimpleMesh sourceMesh;
-	if (!sourceMesh.loadMesh(filenameSource))
-	{
-		std::cout << "Mesh file wasn't read successfully." << std::endl;
-		return;
-	}
-
-	SimpleMesh targetMesh;
-	if (!targetMesh.loadMesh(filenameTarget))
-	{
-		std::cout << "Mesh file wasn't read successfully." << std::endl;
-		return;
-	}
-
-	PointCloud source{sourceMesh};
-	PointCloud target{targetMesh};
-
-	// Search for matches using FLANN.
-	std::unique_ptr<NearestNeighborSearch> nearestNeighborSearch = std::make_unique<NearestNeighborSearchFlann>();
-	nearestNeighborSearch->setMatchingMaxDistance(0.0001f);
-	nearestNeighborSearch->buildIndex(target.getPoints());
-	auto matches = nearestNeighborSearch->queryMatches(source.getPoints());
-
-	// Visualize the correspondences with lines.
-	SimpleMesh resultingMesh = SimpleMesh::joinMeshes(sourceMesh, targetMesh, Matrix4f::Identity());
-	auto sourcePoints = source.getPoints();
-	auto targetPoints = target.getPoints();
-
-	for (unsigned i = 0; i < 100; ++i)
-	{ // sourcePoints.size()
-		const auto match = matches[i];
-		if (match.idx >= 0)
-		{
-			const auto &sourcePoint = sourcePoints[i];
-			const auto &targetPoint = targetPoints[match.idx];
-			resultingMesh = SimpleMesh::joinMeshes(SimpleMesh::cylinder(sourcePoint, targetPoint, 0.002f, 2, 15), resultingMesh, Matrix4f::Identity());
-		}
-	}
-
-	resultingMesh.writeMesh(std::string("correspondences.off"));
-}
-
 int alignBunnyWithICP()
 {
 	// Load the source and target mesh.
