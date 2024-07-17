7fe6d960464abf1957e1be781f19191361c78422
Oleg Magnes <oleg.magnes@gmail.com>
2024-07-17 23:55:43 +0200

    scripts to run all shape icp experiments

---
+++ reproduce/run_linear_partial.sh
@@ -0,0 +1,11 @@
+cd ../build
+make 
+./icp ../reproduce/conf/linear_plane_nn_partial.yaml
+./icp ../reproduce/conf/linear_plane_nn_color_partial.yaml
+./icp ../reproduce/conf/linear_plane_shoot_partial.yaml
+./icp ../reproduce/conf/linear_point_nn_partial.yaml
+./icp ../reproduce/conf/linear_point_nn_color_partial.yaml
+./icp ../reproduce/conf/linear_point_shoot_partial.yaml
+./icp ../reproduce/conf/linear_symm_nn_partial.yaml
+./icp ../reproduce/conf/linear_symm_nn_color_partial.yaml
+./icp ../reproduce/conf/linear_symmetric_shoot_partial.yaml
---
+++ reproduce/run_nonlinear_partial.sh
@@ -0,0 +1,11 @@
+cd ../build
+make 
+./icp ../reproduce/conf/nonlinear_plane_nn_partial.yaml
+./icp ../reproduce/conf/nonlinear_plane_nn_color_partial.yaml
+./icp ../reproduce/conf/nonlinear_plane_shoot_partial.yaml
+./icp ../reproduce/conf/nonlinear_point_nn_partial.yaml
+./icp ../reproduce/conf/nonlinear_point_nn_color_partial.yaml
+./icp ../reproduce/conf/nonlinear_point_shoot_partial.yaml
+./icp ../reproduce/conf/nonlinear_symm_nn_partial.yaml
+./icp ../reproduce/conf/nonlinear_symm_nn_color_partial.yaml
+./icp ../reproduce/conf/nonlinear_symmetric_shoot_partial.yaml
