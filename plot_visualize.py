727946c23d0a5aa54ad0b353cf1331716b8b5f09
suzi-soohyun <soohji03@gmail.com>
2024-07-18 01:02:40 +0200

    update plot visualization script

--- plot_visualize.py
+++ plot_visualize.py
@@ -1,37 +1,52 @@
+import os
 import argparse
 import matplotlib.pyplot as plt
+import numpy as np
+
+# Aliases
+_MAP_NAMES = {
+    "rmse_nn_metric": "RMSE Pointclouds (nearest)",
+    "eval_r": "Frobenius Norm For Rotation",
+    "eval_t": "RMSE Translation",
+}
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--file_path", type=str, default="./metric.txt")
-    parser.add_argument("--type", type=int, default=2, help="0: time, 1: Point-to-Point RMSE, 2: Point-to-Plane RMSE")
-    args = parser.parse_args()
+    path = "C:/TUM/vis/average"
+    folderlist = os.listdir(path)
+    for foldername in folderlist:
+        outputdir = f'C:/TUM/vis/figs/{foldername}'
+        if (os.path.exists(outputdir) == False):
+            os.mkdir(outputdir)
+        
+        nframes = os.listdir(f"{path}/{foldername}")
+        tmp = foldername.split('_')
+        if(len(foldername.split('_')) == 3):
+            icp, linear, correspondence = foldername.split('_')
+            title = f"ICP: {icp} / {linear} / {correspondence}"
+        elif(len(foldername.split('_')) == 4):
+            icp, linear, correspondence, color = foldername.split('_')
+            title = f"ICP: {icp} / {linear} / {correspondence} / {color}"
+       
+        for metric_type in _MAP_NAMES:
+            file_path = f"{path}/{foldername}/{metric_type}.txt"
+            iters, mean_values = [], []
 
-    liter, ltime, lrmse1, lrmse2 = [], [], [], []
-    file_path = args.file_path
-    with open(file_path) as f:
-        lines = f.readlines()
-        for line in lines:
-            iter, time, rmse1, rmse2 = line.split(",")
-            liter.append(int(iter))
-            ltime.append(float(time))
-            lrmse1.append(float(rmse1))
-            lrmse2.append(float(rmse2))
-    
-    if(args.type == 0):
-        data = ltime
-        ylabel = "Time"
-    elif(args.type == 1):
-        data = lrmse1
-        ylabel = "Point To Point RMSE"
-    elif(args.type == 2):
-        data = lrmse2
-        ylabel = "Point To Plane RMSE"
+            with open(file_path) as f:
+                lines = f.readlines()
+                for line in lines:
+                    iter, value = line.split(",")
+                    iters.append(int(iter))
+                    mean_values.append(float(value))
+            
+            mean_values = np.array(mean_values)
+            std_values = mean_values.std()
 
-    xlabel = "Linear ICP"
-    plt.title(xlabel)
-    plt.xlabel('Number of Iterations')
-    plt.ylabel(ylabel)
-    plt.plot(liter, data, c='orange')
-    plt.xticks(liter)
-    plt.show()
+            plt.title(f'{_MAP_NAMES[metric_type]} averaged across all meshes')
+            plt.xlabel("Iteration")
+            plt.ylabel(_MAP_NAMES[metric_type])
+            plt.plot(iters, mean_values)
+            plt.fill_between(iters, mean_values - std_values, mean_values + std_values, alpha=0.2)
+            plt.xticks(iters)
+            plt.savefig(f'{outputdir}/{metric_type}.png') 
+            #plt.show()
+            plt.close()
